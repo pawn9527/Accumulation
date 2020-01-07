e55b5a01ecf254d2f0ff46c8441a25eff413128f
pawn <pawn9527@gmail.com>
2020-01-07 14:38:40 +0800

    改为自己的项目用来当博客写笔记

--- porject/Celery/source/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding:utf8
-"""
-作者:pawn
-邮箱:pawn9537@gmail.com
-日期:2019/1/27
-时间:11:02
-"""
--- porject/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding:utf8
-"""
-作者:pawn
-邮箱:pawn9537@gmail.com
-日期:2019/1/30
-时间:10:08
-"""
